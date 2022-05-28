2a8be67f0c09c0d54541bdec744ba4f93549c9ae
Yin Chu <ycrijnaard@gmail.com>
2022-05-28 18:27:55 +0300

    Added rel attribute

--- components/ButtonSmall.js
+++ components/ButtonSmall.js
@@ -1,6 +1,10 @@
 export default function ButtonSmall(props) {
   return (
-    <a className="rounded-sm bg-neutral-100 px-4 py-2 font-bold text-black" href={props.href} target="_blank">
+    <a
+      className="rounded-sm bg-neutral-100 px-4 py-2 font-bold text-black"
+      href={props.href}
+      target="_blank"
+      rel="noreferrer">
       {props.text}
     </a>
   );
--- components/Footer.js
+++ components/Footer.js
@@ -2,16 +2,24 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-screen space-y-4 bg-blue-700 px-4 py-2 text-center text-neutral-100">
       <span className="m-auto flex w-1/2 items-center justify-between">
-        <a className="cursor-pointer" href="tel:+31636439554" target="_blank">
+        <a className="cursor-pointer" href="tel:+31636439554" target="_blank" rel="noreferrer">
           <i className="ri-phone-fill ri-2x"></i>
         </a>
-        <a className="cursor-pointer" href="https://www.instagram.com/visual_identities_official/" target="_blank">
+        <a
+          className="cursor-pointer"
+          href="https://www.instagram.com/visual_identities_official/"
+          target="_blank"
+          rel="noreferrer">
           <i className="ri-instagram-line ri-2x"></i>
         </a>
-        <a className="cursor-pointer" href="https://www.linkedin.com/company/visual-identities/" target="_blank">
+        <a
+          className="cursor-pointer"
+          href="https://www.linkedin.com/company/visual-identities/"
+          target="_blank"
+          rel="noreferrer">
           <i className="ri-linkedin-fill ri-2x"></i>
         </a>
-        <a className="cursor-pointer" href="mailto:nickcatron@visualidentities.nl" target="_blank">
+        <a className="cursor-pointer" href="mailto:nickcatron@visualidentities.nl" target="_blank" rel="noreferrer">
           <i className="ri-mail-fill ri-2x"></i>
         </a>
       </span>
