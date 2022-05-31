c8404c42f44f4ea83d3d2d8c5bfe1c82b88c639c
Yin Chu <ycrijnaard@gmail.com>
2022-05-31 18:38:08 +0300

    Metatag (1)

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import "remixicon/fonts/remixicon.css";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
@@ -7,6 +9,9 @@ import Layout from "../components/layout";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
