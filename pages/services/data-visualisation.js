43c03bcfd39fe3e85416a0a914fd23e71ba479e7
Yin Chu <ycrijnaard@gmail.com>
2022-06-10 21:25:47 +0300

    InfographicSteps concept

--- pages/services/data-visualisation.js
+++ pages/services/data-visualisation.js
@@ -1,7 +1,10 @@
+import InfographicSteps from "../../public/assets/InfographicSteps";
+
 export default function DataVisualisation() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">This is the Data visualisation subpage, part of the Services folder </h1>
+      {/* <h1 className="text-2xl text-green-500">This is the Data visualisation subpage, part of the Services folder </h1> */}
+      <InfographicSteps />
     </>
   );
 }
